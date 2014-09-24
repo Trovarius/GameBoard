07497bf12ec4bb5baef7ca85dccd8660fd7e6df9
trovarius <tordek.coast@gmail.com>
2014-09-23 22:57:56 -0300

    criando projeto

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+
+
+app.set('views', './views');
+app.set('view engine', 'jade');
+app.set('public', './public');
+app.use(express.static(__dirname + '/public'));
+
+//var port = process.env.PORT || 8080;
+var io = require('socket.io').listen(app.listen(8080));
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+app.get('/chat', function(req, res) {
+  var newId = Math.round(Math.random() * 100000);
+  res.redirect('chat/#'+newId);
+});
+
+app.get('/chat/:id', function(req, res){
+  res.render('chat');
+})
+
+
+io.sockets.on('connection', function(socket){
+
+ socket.on('mensagem', function(data) {
+    io.sockets.emit('client', {id: data['id'], message: data["message"]});
+ });
+
+});
+
+
---
+++ public/js/chat.js
@@ -0,0 +1,16 @@
+var socketio = io.connect();
+var id_chat = window.location.href.match(/#(\d+)/)[0].replace('#', '');
+
+
+socketio.on('client', function(data) {
+  console.log(data);
+  if(data['id'] == id_chat)
+    alert(data['message']);
+});
+
+$(document).ready(function(){
+
+  $('#btnSend').click(function() {
+    socketio.emit('mensagem', {id: id_chat , message: 'teste'});
+  });
+});
