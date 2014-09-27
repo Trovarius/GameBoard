c9b959b38a8dce31ba56711f3667696f58fad873
trovarius <tordek.coast@gmail.com>
2014-09-27 10:10:12 -0300

    recevendo informações do servidor

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,18 +1,33 @@
 var socketio = io.connect();
-var id_chat = window.location.href.match(/(\d+)/)[0];
 
-socketio.on('client', function(data) {
-  console.log(data);
-  if(data['id'] == id_chat)
-    alert(data['message']);
-});
+
+
+function Mensagem(){
+  var url_parse =  window.location.href.match(/game\/(\d+)\/(\w+)/));
+  this.id_chat = url_parse[1];
+  this.player_name = url_parse[2];
+  msg= this;
+
+  this.send = function(msg){
+   socketio.emit('mensagem', {id: this.id_chat, player: this.player_name, message: msg});
+  };
+  
+  this.receive = function(data){
+    if(data['id']== msg.id_chat)
+      alert(data['message']);
+  };
+}
 
 $(document).ready(function(){
+ var msg = new Mensagem();
+
+  socketio.on('client', msg.receive);
+
 
   $('#btnSend').click(function() {
     socketio.emit('mensagem', {id: id_chat , message: 'teste'});
   });
   
-  socketio.emit('mensagem', {id: id_chat, mensage: 'Novo player entrou na sala.'})
+  msg.send('novo player entrou na sala.');
 
 });
